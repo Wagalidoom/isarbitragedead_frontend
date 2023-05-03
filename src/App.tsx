f51eba865a587b52a01cd3ecda56526042bebd20
dassman <lukas-8@live.fr>
2023-05-03 09:12:55 +0200

    - add router - new route to Blocks - new routes for all opportunities

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
 import './styles/globals.css';
 import lightTheme from './styles/theme/lightTheme';
 import Blocks from './components/Blocks';
 import { Box, CssBaseline, Grid, ThemeProvider, Typography } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Filter from './components/Filter';
 import { useState } from 'react';
 import Search from './components/Search';
+import BlockDetails from './components/BlockDetails';
 
 export const LOCAL_IP_ADDRESS = process.env.REACT_APP_LOCAL_IP;
 
@@ -13,25 +14,30 @@ function App() {
   const [searchParams, setSearchParams] = useState<{ searchInput: string | null, profitMin: string | null, profitMax: string | null }>({ searchInput: null, profitMin: null, profitMax: null });
 
   return (
-    <div className="App">
+    <BrowserRouter>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
-        <Box paddingLeft={2} paddingTop={1} sx={{ position: 'fixed', top: 0, left: 0, zIndex: 2 }}>Is arbitrage dead ?</Box>
-        <Grid container columnSpacing={0} sx={{ height: '100%' }}>
-          <Grid item xs={3} md={3} sx={{ backgroundColor: '#f7f1e8', boxShadow: 3, zIndex: 1 }}>
-            <Box sx={{ position: 'sticky', top: '50px' }}>
-              <Filter onSearchChange={setSearchParams} />
-            </Box>
-            <Box sx={{ position: 'fixed', bottom: 0, left: 0, zIndex: 3 }}>
-              <Typography variant='h5'>By Lukas and Jordan</Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={9} md={9}>
-            {(searchParams.searchInput || searchParams.profitMin || searchParams.profitMax) ? <Search searchParams={searchParams} /> : <Blocks />}
-          </Grid>
-        </Grid>
+        <Box paddingLeft={2} paddingTop={1} sx={{ position: 'fixed', top: 0, left: 0, zIndex: 2, fontSize: 'calc(10px + 2vmin)', color: '#454545' }}>Is arbitrage dead ?</Box>
+        <Routes>
+          <Route path="/" element={
+            <Grid container columnSpacing={0} sx={{ height: '100%' }}>
+              <Grid item xs={3} md={3} sx={{ backgroundColor: '#f7f1e8', boxShadow: 3, zIndex: 1 }}>
+                <Box sx={{ position: 'sticky', top: '50px' }}>
+                  <Filter onSearchChange={setSearchParams} />
+                </Box>
+                <Box sx={{ position: 'fixed', bottom: 0, left: 0, zIndex: 3 }}>
+                  <Typography variant='h5'>By Lukas and Jordan</Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={9} md={9}>
+                {(searchParams.searchInput || searchParams.profitMin || searchParams.profitMax) ? <Search searchParams={searchParams} /> : <Blocks />}
+              </Grid>
+            </Grid>} />
+          <Route path="/block/:blockNumber/opportunity/:opportunityIndex" element={<BlockDetails />} />
+        </Routes>
+
       </ThemeProvider>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/BlockDetails.tsx
+++ src/components/BlockDetails.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
-import { BlockData } from './Blocks';
+import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 interface BlockDetailsProps {
-  block: BlockData;
-  onClose: () => void;
 }
 
-const BlockDetails: React.FC<BlockDetailsProps> = ({ block, onClose }) => {
+const BlockDetails: React.FC<BlockDetailsProps> = () => {
+  const { blockNumber, opportunityIndex } = useParams();
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 9999 }} onClick={onClose}>
+    <Box sx={{ display: 'flex', alignItems: 'center', position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 9999 }}>
       <Box sx={{ width: '80%', height: '80%', backgroundColor: '#ffffff', margin: 'auto', padding: 2 }}>
         <Typography variant="h4" sx={{ marginBottom: 2 }}>Block Details</Typography>
-        <Typography variant="h6" sx={{ marginBottom: 1 }}>Block Number: {block.blockNumber}</Typography>
+        <Typography variant="h6" sx={{ marginBottom: 1 }}>Block Number: {blockNumber}</Typography>
         <Typography variant="h6" sx={{ marginBottom: 1 }}>Opportunities:</Typography>
-        {block.opportunities.map((opportunity, index) => (
-          <Typography key={index} variant="body1" sx={{ marginBottom: 1 }}>{JSON.stringify(opportunity)}</Typography>
-        ))}
       </Box>
     </Box>
   );
