d4c8300b96e3582053b5cd7edbbff20ef4fff029
Jinter Laptop <jordan.cosio@outlook.fr>
2023-05-04 15:33:20 +0200

    - Added statistics displayed from api request on component refresh

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -3,6 +3,7 @@ import React, { ChangeEvent, useState, KeyboardEvent, useEffect } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+import { LOCAL_IP_ADDRESS } from '../App';
 export interface ISidePanel {
   onSearchChange: (searchParams: { searchInput: string | null, profitMin: string | null, profitMax: string | null }) => void,
   currentBlock: number
@@ -11,7 +12,12 @@ export interface ISidePanel {
 interface IArbStatistics {
   totalProfit: number,
   nbOpportunities: number,
-  mostArbitragedToken: string
+  mostArbitragedToken: {
+    tokenAddress: string,
+    tokenName: string,
+    tokenSymbol: string,
+    tokenLogo: string,
+  }
 }
 
 const Filter: React.FC<ISidePanel> = ({ onSearchChange, currentBlock }) => {
@@ -19,25 +25,28 @@ const Filter: React.FC<ISidePanel> = ({ onSearchChange, currentBlock }) => {
   const [profitMin, setProfitMin] = useState<string | null>(null);
   const [profitMax, setProfitMax] = useState<string | null>(null);
   const [hasSearched, setHasSearched] = useState(false);
-  const [apiData, setApiData] = useState<IArbStatistics>({
-    totalProfit: 0,
-    nbOpportunities: 0,
-    mostArbitragedToken: ""
-  });
+  const [apiData, setApiData] = useState<IArbStatistics | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // Simulate API calls to fetch data
+  // Appels à l'API pour avoir les stats des tokens
   useEffect(() => {
-    // Fetch block number and other data here
-    // For now, we use dummy data with a delay of 5000ms (5 seconds)
-    setTimeout(() => {
-      setApiData({
-        totalProfit: 12345.67,
-        nbOpportunities: 100,
-        mostArbitragedToken: 'ABC',
-      });
-      setIsLoading(false);
-    }, 5000);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://${LOCAL_IP_ADDRESS}:3001/api/stats`);
+        const data = await response.json();
+        setApiData({
+          totalProfit: data.totalPotentialProfit,
+          nbOpportunities: data.numberOfOpportunities,
+          mostArbitragedToken: data.mostArbitragedToken
+        });
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
 
@@ -152,7 +161,7 @@ const Filter: React.FC<ISidePanel> = ({ onSearchChange, currentBlock }) => {
             marginTop: 2,
             color: 'black',
             position: 'fixed',
-            bottom: '15%',
+            bottom: '10%',
             width: '100%',
             zIndex: 2,
           }}
@@ -163,18 +172,31 @@ const Filter: React.FC<ISidePanel> = ({ onSearchChange, currentBlock }) => {
               Block  <b>{currentBlock !== 0 ? currentBlock : '...'}</b>
             </Typography>
           </Box>
-          <Typography variant="subtitle1" component="div" sx={{ fontSize: '2.5rem', fontWeight: 'bold', padding:2}}>
+          <Typography variant="subtitle1" component="div" sx={{ fontSize: '2.5rem', fontWeight: 'bold', padding: 2 }}>
             Today
           </Typography>
-          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding:1 }} >
-            Total potential profit: <b>{isLoading ? <CircularProgress size={20} /> :`${apiData.totalProfit} $`}</b>
+          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding: 1 }} >
+            Total potential profit: <b>{isLoading ? <CircularProgress size={20} /> : `${apiData?.totalProfit.toFixed(2)} $`}</b>
           </Typography>
-          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding:1 }}>
-            Number of opportunities: <b>{isLoading ? <CircularProgress size={20} /> : apiData.nbOpportunities}</b>
+          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding: 1 }}>
+            Number of opportunities: <b>{isLoading ? <CircularProgress size={20} /> : apiData?.nbOpportunities}</b>
           </Typography>
-          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding:1 }}>
-            Most arbitraged token: <b>{isLoading ? <CircularProgress size={20} /> : apiData.mostArbitragedToken}</b>
+          <Typography variant="body1" component="div" sx={{ fontSize: '1.45rem', padding: 1 }}>
+            Most arbitraged token:
+            <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 1 }}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <b>{isLoading ? <CircularProgress size={20} /> : apiData?.mostArbitragedToken.tokenName}</b>
+                {apiData?.mostArbitragedToken.tokenLogo && (
+                  <Box sx={{ marginLeft: 1 }}>
+                    <img src={apiData?.mostArbitragedToken.tokenLogo} alt="top-token-logo" width="20" height="20" />
+                  </Box>
+                )}
+              </Box>
+            </Box>
           </Typography>
+
+
+
 
         </Box>
       </Box>
