f6c26bed5b2a3f35f677139f59605bcb57269427
Jinter Laptop <jordan.cosio@outlook.fr>
2023-05-15 20:05:48 +0200

    - Added title link to root - Centered title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/globals.css';
 import lightTheme from './styles/theme/lightTheme';
 import Blocks from './components/Blocks';
 import { Box, CssBaseline, Grid, ThemeProvider, useMediaQuery } from '@mui/material';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SidePanel from './components/SidePanel';
 import { useState } from 'react';
 import Search from './components/Search';
@@ -19,9 +19,11 @@ function App() {
     <BrowserRouter>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
-        <Box paddingLeft={2} paddingTop={1} sx={{ position: 'fixed', top: 0, left: 0, zIndex: 2, fontSize: 'calc(10px + 2vmin)', color: '#454545' }}>Is arbitrage dead ?</Box>
         <Grid container columnSpacing={0} sx={{ width: '100%', height: '100%' }}>
-          <Grid item xs={0} sm={3} md={3} sx={{ position: 'sticky', paddingTop: '50px', top: 0, height: '100vh', backgroundColor: '#f7f1e8', boxShadow: 3, zIndex: 1, display: isSmallScreen ? 'none' : 'block' }}>
+          <Grid item xs={0} sm={3} md={3} sx={{ position: 'sticky', paddingTop: '20px', top: 0, height: '100vh', backgroundColor: '#f7f1e8', boxShadow: 3, zIndex: 1, display: isSmallScreen ? 'none' : 'block' }}>
+            <Box sx={{ textAlign: 'center', fontSize: 'calc(10px + 2vmin)', color: '#454545' }}>
+              <Link to="/" style={{ textDecoration: 'none', color: '#454545' }}>Is arbitrage dead ?</Link>
+            </Box>
             <SidePanel onSearchChange={setSearchParams} currentBlock={currentBlockNumber} />
           </Grid>
           <Grid item xs={12} sm={9} md={9} sx={{ backgroundColor: '#eae6e1' }}>
