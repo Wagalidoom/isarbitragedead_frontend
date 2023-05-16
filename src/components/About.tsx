efe6a9c1a9869822a7ce85a25f736c6c223ee3f8
Jinter Laptop <jordan.cosio@outlook.fr>
2023-05-16 17:43:25 +0200

    - CSS styles on about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Typography, Box } from '@mui/material';
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>Ce site a été créé pour ... </p> {/* Ajoutez votre texte ici */}
-    </div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <Typography variant="h2" sx={{ textAlign: 'center', color: '#000', marginTop: '10vh' }}>About</Typography>
+      <Box sx={{ width: '70%', marginTop: '20px', padding: '20px' }}>
+        <Typography sx={{ color: '#000', fontSize: '17px' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
