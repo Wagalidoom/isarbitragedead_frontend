5f07a5498451cbf3062c07d53e586b35f2ef21b6
Jinter Laptop <jordan.cosio@outlook.fr>
2023-05-22 20:59:28 +0200

    - Added profile cards - Added donate section - Replaced github button by hyperlink

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,7 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Link } from '@mui/material';
 import { AwesomeButtonSocial } from "react-awesome-button";
 import { useTheme } from '@mui/material/styles';
+import ProfileCard from './ProfileCard';
 
 const About = () => {
   const theme = useTheme();
@@ -12,36 +13,58 @@ const About = () => {
           <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
             What is this website?
           </Typography>
-          <Typography sx={{ color: theme.palette.text.secondary, fontSize: 17,  marginBottom: 4 }} align="justify" gutterBottom>
-            This is a website showcasing the fruits of our labor: the opportunities identified by our real-time arbitrage bot. It is developed in NodeJS, integrating mathematical and statistical techniques, artificial intelligence, and of course, interactions with Web3. Although theoretically exploitable, we do not advise trying to take advantage of the opportunities displayed on this site. Indeed, many players compete to seize these opportunities and have optimized infrastructures. We emphasize that these results are publicly shared for educational purposes, and we are not responsible for any derivative use of this site.
+          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17, marginBottom: 4 }} align="justify" gutterBottom>
+            This website showcases the fruits of our labor: the opportunities identified by our real-time arbitrage bot. It is developed in NodeJS, integrating mathematical and statistical techniques, artificial intelligence, and of course, interactions with Web3. Although theoretically exploitable, we do not advise trying to take advantage of the opportunities displayed on this site. Indeed, many players compete to seize these opportunities and have optimized infrastructures. We emphasize that these results are publicly shared for educational purposes, and we are not responsible for any derivative use of this site.
           </Typography>
           <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
             Who are we?
           </Typography>
-          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17,  marginBottom: 4 }} align="justify" gutterBottom>
+          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17, marginBottom: 4 }} align="justify" gutterBottom>
             We are two freelancers, Lukas and Jordan, operating in Web3 and Artificial Intelligence, respectively. We aim to improve this bot to capture a wider spectrum of MEV related to arbitrage. Currently, our bot broadcasts "SingleHop" opportunities (ie: only passing through one intermediate token). A cyclic arbitrage solution with N hops is under development, with the intention to roll it out once a stable version is ready. In the future, we plan to extend the number of exchanges considered in our calculations.
           </Typography>
           <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
             What is arbitrage?
           </Typography>
-          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17,  marginBottom: 4 }} align="justify" gutterBottom>
+          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17, marginBottom: 4 }} align="justify" gutterBottom>
             Arbitrage is a MEV strategy that involves profiting from price differences between different markets for the same asset. To put it simply, it's about buying an asset at a lower price on one market, then simultaneously selling it at a higher price on another market, thus realizing a risk-free profit. In our case, our arbitrage bot scans real-time price differences between various cryptocurrency exchange platforms and highlights arbitrage opportunities.
           </Typography>
           <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
             Contact
           </Typography>
-          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17,  marginBottom: 4 }} align="justify" gutterBottom>
+          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17, marginBottom: 4 }} align="justify" gutterBottom>
             We maintain this arbitrage bot thanks to the income generated from freelance missions. If you have needs in the domains of Web3, AI or ChatGPT, we're just an email away:<br /><br />
-            <Typography sx={{ color: theme.palette.text.primary, fontSize: 17 }}>
-              <b><u> Lukas B.</u>  <i>Freelance in Web3, NFT, Blockchain Dev: maillukas</i></b><br />
-              <b><u>Jordan C.</u> Freelance in AI, Computer Vision and ChatGPT Expert: bouton</b>
-            </Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: '10px' }}>
+              <ProfileCard
+                imageUrl="https://avatars.githubusercontent.com/u/43578243?v=4"
+                name="Lukas B."
+                title="Freelance"
+                domain="Web3 Designer & Blockchain Developer"
+                linkedin="https://www.linkedin.com/in/lukas-balandraux-677693150/"
+                github="https://github.com/Wagalidoom"
+                email="lukasbalandraux@gmail.com"
+              />
+              <ProfileCard
+                imageUrl="https://avatars.githubusercontent.com/u/45495815?v=4"
+                name="Jordan C."
+                title="Freelance"
+                domain="AI & Computer Vision developer, ChatGPT Expert"
+                linkedin="https://www.linkedin.com/in/jorc/"
+                github="https://github.com/Jinter410"
+                email="jordan.cosio@outlook.fr"
+              />
+            </Box>
           </Typography>
           <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
             Sources
           </Typography>
+          <Typography sx={{ color: theme.palette.text.primary, fontSize: 17, marginBottom: 4 }} align="justify" gutterBottom>
+            Frontend sources are available on <Link href="https://github.com/Wagalidoom/isarbitragedead_frontend" sx={{ color: 'inherit' }}>Github !</Link>
+          </Typography>
+          <Typography sx={{ color: theme.palette.text.secondary }} variant="h4" align="justify" gutterBottom>
+            Support us
+          </Typography>
           <Typography sx={{ color: theme.palette.text.primary, fontSize: 17 }} align="justify" gutterBottom>
-            Frontend sources are available on GitHub : <AwesomeButtonSocial type="github" href="https://github.com/Wagalidoom/isarbitragedead_frontend"><div/></AwesomeButtonSocial>
+            On ETH mainnet : 0xBd7f5D11dAA619e0169Cb07DC45b3a7898f03533
           </Typography>
         </div>
       </Box>
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,74 @@
+import { Box, Typography, Avatar, IconButton, useTheme } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
+import { styled } from "@mui/system";
+
+interface IProfileCard {
+    imageUrl: string;
+    name: string;
+    title: string;
+    domain: string;
+    linkedin: string;
+    github: string;
+    email: string;
+}
+
+const ProfileCard: React.FC<IProfileCard> = ({ imageUrl, name, title, domain, linkedin, github, email }) => {
+    const theme = useTheme();
+
+    const CardContainer = styled(Box)({
+        display: "grid",
+        gridTemplateColumns: "auto 1fr auto",
+        gridTemplateRows: "repeat(3, auto)",
+        alignItems: "center",
+        gap: 1,
+        backgroundColor: theme.colors.backgroundSides,
+        borderRadius: "10px",
+        marginBottom: "10px",
+        padding: '10px',
+        paddingRight: '20px',
+        paddingLeft: '20px',
+        width: '50%',
+        margin: 'auto'
+    });
+
+    const PersonInfo = styled(Box)({
+        display: "flex",
+        alignItems: "center",
+    });
+
+    const PersonDetails = styled(Box)({
+        marginLeft: "20px",
+    });
+
+    const IconContainer = styled(Box)({
+        marginLeft: "20px"
+    });
+
+    return (
+        <CardContainer>
+            <PersonInfo>
+                <Avatar src={imageUrl} alt={name} sx={{ width: 70, height: 70 }} />
+                <PersonDetails>
+                    <Typography variant="h6" fontWeight={'bold'}><u>{name}</u></Typography>
+                    <Typography variant="subtitle1" fontWeight={'bold'}>{title}</Typography>
+                    <Typography variant="subtitle2">{domain}</Typography>
+                </PersonDetails>
+            </PersonInfo>
+            <IconContainer>
+                <IconButton onClick={() => window.open(linkedin, "_blank")}>
+                    <LinkedInIcon />
+                </IconButton>
+                <IconButton onClick={() => window.open(github, "_blank")}>
+                    <GitHubIcon />
+                </IconButton>
+                <IconButton onClick={() => window.open(`mailto:${email}`, "_blank")}>
+                    <EmailIcon />
+                </IconButton>
+            </IconContainer>
+        </CardContainer>
+    );
+};
+
+export default ProfileCard;
